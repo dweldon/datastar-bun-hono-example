c119842fe0f856ed46eb61d10f15d4d6b8de738a
David Weldon <dweldon@gmail.com>
2025-10-15 19:07:58 -0400

    Use a specific version of the Datastar bundle

--- src/client/Page.tsx
+++ src/client/Page.tsx
@@ -18,7 +18,7 @@ export const Page: FC<PropsWithChildren> = ({ children }) => (
       </main>
       <script
         type="module"
-        src="https://cdn.jsdelivr.net/gh/starfederation/datastar@main/bundles/datastar.js"
+        src="https://cdn.jsdelivr.net/gh/starfederation/datastar@1.0.0-RC.5/bundles/datastar.js"
       ></script>
     </body>
   </html>
